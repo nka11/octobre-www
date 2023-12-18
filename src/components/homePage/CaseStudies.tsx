2d2106bf4242f9a585a45862f4eec47cde7a51ea
Ethan Fajnkuchen <102037640+EthanFajnkuchen@users.noreply.github.com>
2023-12-18 16:10:39 +0200

    Inverse block 3 and 4 in case studies section

--- src/components/homePage/CaseStudies.tsx
+++ src/components/homePage/CaseStudies.tsx
@@ -96,44 +96,10 @@ const CaseStudies = () => {
                     </div>
                 </div>
             </div>
-            <div className="case_study row justify-content-center" property="schema:review" typeof="schema:Review" resource="#review-bramding">
+            <div className="case_study row justify-content-center" property="schema:review" typeof="schema:Review" resource="#review-conformite">
                 <div className="col-lg-6 col-md-12 col-sm-12 mb-4 mt-lg-4 spe-col">
                     <div className="card case-study-large-card h-100 case-3 d-flex flex-column">
                         <div className="card-body mt-auto">
-                            <div className="stats" property="schema:reviewBody">
-                                <div className="card-title" property="schema:keywords">UNE ÉLIGIBILITÉ SUR</div>
-                                <strong className="card-text-number">40%</strong>
-                                <div className="card-text">des fonctions supports et commerciales.</div>
-                            </div>
-                        </div>
-                    </div>
-                    
-                </div>
-                <div className="col-lg-6 col-md-12 col-sm-12 mb-lg-4 mt-4 spe-col">
-                    <div className="card case-study-small-card h-100 case-3">
-                        <div className="card-body" property="schema:itemReviewed" typeof="schema:Thing" resource="#bramding">
-                            <div className="card-title" property="schema:name">MARQUE EMPLOYEUR 3.0</div>
-                            <div className="auteur" property="schema:author" typeof="schema:Person">
-                                <div className="photo-auteur">
-                                <img src="/assets/photos/profil-pic.png" />
-                                </div>
-                                <div className="infos-auteur" property="schema:name">
-                                    <div id="new" className='card-text' property="schema:givenName">Xavier</div>
-                                    <div id="new" className='card-text' property="schema:jobTitle">Directeur des ressources humaines</div>
-                                </div>
-                            </div>
-                            <div id="avis" property="schema:memberOf" typeof="schema:Organization">
-                                <div id='title-avis' className="card-title" property="schema:name">ETI dans le domaine de la logistique</div>
-                                <div id='body-avis' className="card-text" property="schema:reviewBody">Notre nouvelle campagne de communication a été intégrée à notre CRM ce qui a fortement impacté notre <span property="schema:keywords">processus de recrutement</span> et notre <span property="schema:keywords">expérience candidats.</span></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="case_study row justify-content-center" property="schema:review" typeof="schema:Review" resource="#review-conformite">
-                <div className="col-lg-6 col-md-12 col-sm-12 mb-4 mt-lg-4 spe-col">
-                    <div className="card case-study-large-card h-100 case-4 d-flex flex-column">
-                        <div className="card-body mt-auto align-self-end">
                             <div className="stats" property="schema:reviewBody">
                                 <div className="card-title">UNE ÉLIGIBILITÉ SUR</div>
                                 <strong className="card-text-number">91%</strong>
@@ -143,7 +109,7 @@ const CaseStudies = () => {
                     </div>      
                 </div>
                 <div className="col-lg-6 col-md-12 col-sm-12 mb-lg-4 mt-4 spe-col">
-                    <div className="card case-study-small-card h-100 case-4">
+                    <div className="card case-study-small-card h-100 case-3">
                         <div className="card-body">
                             <div className="card-title"  property="schema:itemReviewed" typeof="schema:Thing" resource="#conformite">
                                 <span property="schema:name">PERFORMANCE SÉCURISÉE / CYBER-HYGIÈNE</span>
@@ -170,7 +136,42 @@ const CaseStudies = () => {
                         </div>
                     </div>
                 </div>
-            </div>            
+            </div> 
+            <div className="case_study row justify-content-center" property="schema:review" typeof="schema:Review" resource="#review-bramding">
+                <div className="col-lg-6 col-md-12 col-sm-12 mb-4 mt-lg-4 spe-col">
+                    <div className="card case-study-large-card h-100 case-4 d-flex flex-column">
+                        <div className="card-body mt-auto align-self-end">
+                            <div className="stats" property="schema:reviewBody">
+                                <div className="card-title" property="schema:keywords">UNE ÉLIGIBILITÉ SUR</div>
+                                <strong className="card-text-number">40%</strong>
+                                <div className="card-text">des fonctions supports et commerciales.</div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div className="col-lg-6 col-md-12 col-sm-12 mb-lg-4 mt-4 spe-col">
+                    <div className="card case-study-small-card h-100 case-4">
+                        <div className="card-body" property="schema:itemReviewed" typeof="schema:Thing" resource="#bramding">
+                            <div className="card-title" property="schema:name">MARQUE EMPLOYEUR 3.0</div>
+                            <div className="auteur" property="schema:author" typeof="schema:Person">
+                                <div className="photo-auteur">
+                                <img src="/assets/photos/profil-pic.png" />
+                                </div>
+                                <div className="infos-auteur" property="schema:name">
+                                    <div id="new" className='card-text' property="schema:givenName">Xavier</div>
+                                    <div id="new" className='card-text' property="schema:jobTitle">Directeur des ressources humaines</div>
+                                </div>
+                            </div>
+                            <div id="avis" property="schema:memberOf" typeof="schema:Organization">
+                                <div id='title-avis' className="card-title" property="schema:name">ETI dans le domaine de la logistique</div>
+                                <div id='body-avis' className="card-text" property="schema:reviewBody">Notre nouvelle campagne de communication a été intégrée à notre CRM ce qui a fortement impacté notre <span property="schema:keywords">processus de recrutement</span> et notre <span property="schema:keywords">expérience candidats.</span></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+           
             <div className="row octobre_row text-center">
                 <div className="col">
                     <button className="octobre_black" style={{display: 'none'}}>Voir + d’étude de cas</button>
