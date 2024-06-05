67e559a0c0cb20b4b1979394700662e771f226cb
Nicolas Karageuzian <nkarageuzian@gmail.com>
2024-06-05 23:59:26 +0200

    Add some htaccess seo optimizations (canonical)

--- v2/pages/about/+Page.vue
+++ v2/pages/about/+Page.vue
@@ -1,57 +1,63 @@
 <template>
   <div class="container flex flex-col w-max">
 
-    <h2 class="text-center text-3xl">Mentions légales</h2>
-          <h3 class="text-2xl py-6">Éditeur du site :</h3>
-          <p>
-              85 Conseil et Recrutement <br/>
-              SAS, <br/>
-              3 Rue Hippolyte Pinson, <br/>
-              94340, Joinville-le-Pont.<br/>
-              Creteil B 884 728 106,<br/>
-              Capital social, 1 000,00 €.
-          </p>
-          <h3 class="text-2xl py-6">Directeur de la publication :</h3>
-          <p>
-              Samuel Assoun,<br/>
-              3 Rue Hippolyte Pinson, <br/>
-              94340, Joinville-le-Pont.
-          </p>
-          <h3 class="text-2xl py-6">Hébergeur du site :</h3>
-          <p>
-              o2switch<br/>
-              Chem. des Pardiaux,<br/>
-              63000 Clermont-Ferrand<br/>
-              support@o2switch.fr<br/>
-              04 44 44 60 40<br/>
-          </p>
-          <h3 class="text-2xl py-6">Données personnelles</h3>
-          <p>
-              Conformément à la loi "Informatique et Libertés" du 6 janvier 1978 modifiée,
-              et au Règlement Général sur la Protection des Données (RGPD), vous disposez d'un droit d'accès,
-              de rectification et de suppression des données vous concernant.
-              Pour exercer ce droit, veuillez contacter contact@octobre.tech.
-          </p>
-          <h3 class="text-2xl py-6">Cookies :</h3>
-          <p>
-              Ce site n’utilise que les cookies nécessaire a son fonctionnement, il n’y a pas de cookies tiers. En naviguant sur ce site, vous acceptez l'utilisation de ces cookies indispensable. 
-          </p>
-          <h3 class="text-2xl py-6">Propriété intellectuelle :</h3>
-          <p>
-              L'ensemble de ce site relève de la législation française et internationale sur le droit d'auteur et la propriété intellectuelle. Tous les droits de reproduction sont réservés, y compris pour les documents téléchargeables et les représentations iconographiques et photographiques.
-          </p>
-          <h3 class="text-2xl py-6">Responsabilité :</h3>
-          <p>
-              L'éditeur du site ne peut être tenu responsable des erreurs, inexactitudes ou omissions présentes sur le site, ni des éventuels préjudices résultant de l'utilisation des informations fournies.
-          </p>
-          <h3 class="text-2xl py-6">Contact :</h3>
-          <p>
-              Pour toute question ou demande d'information concernant le site, veuillez nous contacter à l'adresse suivante : conseil@octobre.tech
-          </p>
-          <h3 class="text-2xl py-6">Loi applicable :</h3>
-          <p>
-              Le présent site est soumis à la loi française.
-          </p>
+    <h2 class="text-center text-3xl p-12">Mentions légales</h2>
+        <div class="flex">
+            <div class="basis-2/3 p-8 text-justify bg-octobre-expertise-intro-bg">
+              <h3 class="text-2xl py-6">Données personnelles</h3>
+              <p>
+                  Conformément à la loi "Informatique et Libertés" du 6 janvier 1978 modifiée,
+                  et au Règlement Général sur la Protection des Données (RGPD), vous disposez d'un droit d'accès,
+                  de rectification et de suppression des données vous concernant.
+                  Pour exercer ce droit, veuillez contacter contact@octobre.tech.
+              </p>
+              <h3 class="text-2xl py-6">Cookies :</h3>
+              <p>
+                  Ce site n’utilise que les cookies nécessaire a son fonctionnement, il n’y a pas de cookies tiers. En naviguant sur ce site, vous acceptez l'utilisation de ces cookies indispensable. 
+              </p>
+              <h3 class="text-2xl py-6">Propriété intellectuelle :</h3>
+              <p>
+                  L'ensemble de ce site relève de la législation française et internationale sur le droit d'auteur et la propriété intellectuelle. Tous les droits de reproduction sont réservés, y compris pour les documents téléchargeables et les représentations iconographiques et photographiques.
+              </p>
+              <h3 class="text-2xl py-6">Responsabilité :</h3>
+              <p>
+                  L'éditeur du site ne peut être tenu responsable des erreurs, inexactitudes ou omissions présentes sur le site, ni des éventuels préjudices résultant de l'utilisation des informations fournies.
+              </p>
+              <h3 class="text-2xl py-6">Contact :</h3>
+              <p>
+                  Pour toute question ou demande d'information concernant le site, veuillez nous contacter à l'adresse suivante : conseil@octobre.tech
+              </p>
+              <h3 class="text-2xl py-6">Loi applicable :</h3>
+              <p>
+                  Le présent site est soumis à la loi française.
+              </p>
+            </div>
+            <div class="basis-1/3 pl-6">
+                <h3 class="text-2xl py-6">Éditeur du site :</h3>
+                <p>
+                    85 Conseil et Recrutement <br/>
+                    SAS, <br/>
+                    3 Rue Hippolyte Pinson, <br/>
+                    94340, Joinville-le-Pont.<br/>
+                    Creteil B 884 728 106,<br/>
+                    Capital social, 1 000,00 €.
+                </p>
+                <h3 class="text-2xl py-6">Directeur de la publication :</h3>
+                <p>
+                    Samuel Assoun,<br/>
+                    3 Rue Hippolyte Pinson, <br/>
+                    94340, Joinville-le-Pont.
+                </p>
+                <h3 class="text-2xl py-6">Hébergeur du site :</h3>
+                <p>
+                    o2switch<br/>
+                    Chem. des Pardiaux,<br/>
+                    63000 Clermont-Ferrand<br/>
+                    support@o2switch.fr<br/>
+                    04 44 44 60 40<br/>
+                </p>
+            </div>
+        </div>  
   </div>
 </template>
 
